d8e064fbad821aa33d333551c3f2a64e6b7701a4
Yordan Gospodinov <jordang_varna@abv.bg>
2019-10-29 13:32:58 +0200

    Initial

---
+++ Cocktail_Magician/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Cocktail_Magician.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
