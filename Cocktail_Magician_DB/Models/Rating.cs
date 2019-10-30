3d4681f8ba3d001b3f321696c917366e64137586
NicoRan <nicorangelov@gmail.com>
2019-10-30 21:37:53 +0200

    identity fixed

---
+++ Cocktail_Magician_DB/Models/Rating.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Cocktail_Magician_DB.Models
+{
+    public class Rating
+    {
+        public string Id { get; set; }
+        public double Rate { get; set; }
+    }
+}
