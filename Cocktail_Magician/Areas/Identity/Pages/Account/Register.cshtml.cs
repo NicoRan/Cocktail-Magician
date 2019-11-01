7a85cd3c66cd74afced3ae787ea041bb10980471
Yordan Gospodinov <jordang_varna@abv.bg>
2019-11-02 01:08:01 +0200

    Created LoginAndRegisterModel Implemented Login and Register buttons Created Partial Views Login and Register

--- Cocktail_Magician/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Cocktail_Magician/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -71,7 +71,7 @@ namespace Cocktail_Magician.Areas.Identity.Pages.Account
             returnUrl = returnUrl ?? Url.Content("~/");
             if (ModelState.IsValid)
             {
-                var user = new User { UserName = Input.Email, Email = Input.Email };
+                var user = new User { UserName = Input.Username, Email = Input.Email };
                 var result = await _userManager.CreateAsync(user, Input.Password);
                 if (result.Succeeded)
                 {
---
+++ Cocktail_Magician/Models/LoginAndRegisterModel.cs
@@ -0,0 +1,14 @@
+﻿using Cocktail_Magician.Areas.Identity.Pages.Account;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cocktail_Magician.Models
+{
+    public class LoginAndRegisterModel
+    {
+        public LoginModel Login { get; set; }
+        public RegisterModel Register { get; set; }
+    }
+}
