73035a5c795227b29e719b9701bed0f3f040ef81
NicoRan <nicorangelov@gmail.com>
2019-11-13 01:01:02 +0200

    Conficts resoved Review page and ReviewViewModel added

---
+++ Cocktail_Magician/Areas/BarCrower/Models/ReviewVeiwModel.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cocktail_Magician.Areas.BarCrower.Models
+{
+    public class ReviewVeiwModel
+    {
+        public string Id { get; set; }
+        [Display(Name="Comment")]
+        public string Commentar { get; set; }
+        public int Rating { get; set; }
+    }
+}
