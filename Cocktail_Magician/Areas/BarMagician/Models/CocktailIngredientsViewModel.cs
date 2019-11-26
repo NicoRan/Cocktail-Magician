603924d847fa1d1b936fcd6c413b67c18e74b68d
Yordan Gospodinov <jordang_varna@abv.bg>
2019-11-26 03:13:08 +0200

    Added EditCocktailViewModel and its Mappers

---
+++ Cocktail_Magician/Areas/BarMagician/Models/CocktailIngredientsViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace Cocktail_Magician.Areas.BarMagician.Models
+{
+    public class CocktailIngredientsViewModel
+    {
+        public string CocktailId { get; set; }
+        public string IngredientId { get; set; }
+        public string IngredientName { get; set; }
+    }
+}
---
+++ Cocktail_Magician/Infrastructure/Mappers/CocktailIngredientViewModelMapper.cs
@@ -0,0 +1,46 @@
+﻿using Cocktail_Magician.Areas.BarMagician.Models;
+using Cocktail_Magician_Services.DTO;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Cocktail_Magician.Infrastructure.Mappers
+{
+    public static class CocktailIngredientViewModelMapper
+    {
+        public static CocktailIngredientDTO ToDTO(this CocktailIngredientsViewModel viewModel)
+        {
+            var cocktailIngredientDTO = new CocktailIngredientDTO
+            {
+                CocktailId = viewModel.CocktailId,
+                IngredientId = viewModel.IngredientId,
+                IngredientName = viewModel.IngredientName
+            };
+            return cocktailIngredientDTO;
+        }
+
+        public static CocktailIngredientsViewModel ToVM(this CocktailIngredientDTO cocktailIngredientDTO)
+        {
+            var viewModel = new CocktailIngredientsViewModel
+            {
+                CocktailId = cocktailIngredientDTO.CocktailId,
+                IngredientId = cocktailIngredientDTO.IngredientId,
+                IngredientName = cocktailIngredientDTO.IngredientName
+            };
+            return viewModel;
+        }
+
+        public static ICollection<CocktailIngredientDTO> ToDTO(this ICollection<CocktailIngredientsViewModel> viewModels)
+        {
+            var newCollection = viewModels.Select(c => c.ToDTO()).ToList();
+            return newCollection;
+        }
+
+        public static ICollection<CocktailIngredientsViewModel> ToVM(this ICollection<CocktailIngredientDTO> cocktails)
+        {
+            var newCollection = cocktails.Select(c => c.ToVM()).ToList();
+            return newCollection;
+        }
+    }
+}
